8994da2e937844c643f32226a38efddc2b3895f8
fallsky0919 <fallsky0919@gmail.com>
2014-08-09 17:55:58 +0900

    첫커밋

---
+++ src/mina1/mina1.java
@@ -0,0 +1,17 @@
+package mina1;
+
+public class mina1 {
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		System.out.println(" local ori1");
+		
+		System.out.println(" local ori2");
+		
+		
+		
+	}
+
+}
