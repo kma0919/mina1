b984a40cca1f2e0e19e75b6cac7909c499df7182
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 15:19:05 +0900

    1212

---
+++ src/mina1/mina2.java
@@ -0,0 +1,14 @@
+package mina1;
+
+public class mina2 {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+				
+		System.out.println(" local ori1");
+		
+
+	}
+
+}
