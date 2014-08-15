d1c7c85ac3eda620cddd6361cad0924372dad616
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 15:22:17 +0900

    commit 20140815

--- src/mina1/mina2.java
+++ src/mina1/mina2.java
@@ -6,7 +6,7 @@ public class mina2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
-		System.out.println(" local ori1");
+		System.out.println(" local ori123");
 		
 
 	}
