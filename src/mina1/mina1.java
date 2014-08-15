87f618e9872575b8b9a5286edcf0fb571816aee1
kma0919 <kma0919@gmail.com>
2014-08-15 15:20:59 +0900

    Update mina1.java

--- src/mina1/mina1.java
+++ src/mina1/mina1.java
@@ -17,20 +17,12 @@ public class mina1 {
 		System.out.println(" local ori1 서버 추가");
 		
 		System.out.println(" local ori24444");
-		System.out.println(" local ori33");
-		System.out.println(" local orisddsdsd33");
 		
-		System.out.println("`1112222222");
 		
-		System.out.println(" server 추가1");
 		
-		System.out.println(" server 추가1");
-		System.out.println("`1112222222");
-		
-		System.out.println(" server 추가1");
-		System.out.println("`1112222222");
-		
-		System.out.println(" server 추가1");
+		//서버 추가 20140815
+		System.out.println(" server add 1");
+	
 
 	}
 
