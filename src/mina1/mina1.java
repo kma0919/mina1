e0393a88b67b178a16bc8ed117aa878c92d7c09d
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 19:04:00 +0900

    aedfesdgf

--- src/mina1/mina1.java
+++ src/mina1/mina1.java
@@ -6,31 +6,9 @@ public class mina1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-
-//<<<<<<< HEAD
-//<<<<<<< HEAD
-		System.out.println(" hoho 브랜치 덮기sssss");
-//=======
-		System.out.println(" hoho 브랜치 덮기");
-//>>>>>>> refs/remotes/origin/hoho
-//민블 브랜치 덮고싶어용~
-
-//=======
-		System.out.println(" local ori1 서버 추가");
+//sssssssssssssssssssssssssssssssssssssssssssssssssssss
 		
-		System.out.println(" local ori24444");
-		System.out.println(" local ori33");
-		System.out.println(" local orisddsdsd33");
-		
-		System.out.println("`1112222222");
-		
-		System.out.println(" server 추가1");
-		
-		System.out.println(" server 추가1");
-		System.out.println("`1112222222");
-		
-		//새로운 브런치 입니당당당
-//>>>>>>> refs/remotes/origin/minble
+		System.out.println(" brancsdfsdfsdt1");
 
 	}
 
