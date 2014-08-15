eabd763d518011d76b6bb7ebd2a672b00cd8d488
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 16:13:29 +0900

    브랜치병합

--- src/mina1/mina2.java
+++ src/mina1/mina2.java
@@ -6,9 +6,13 @@ public class mina2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
+
 		System.out.println(" local ori2");
 		
 		System.out.println(" server add 1");
+
+		System.out.println(" local ori123");
+
 		
 
 	}
