0ad82f0f8b86d1abd5acb3514d1c49f3aa4d3253
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 19:04:50 +0900

    sdfsdfeeeeeee

--- src/mina1/mina1.java
+++ src/mina1/mina1.java
@@ -7,7 +7,7 @@ public class mina1 {
 		// TODO Auto-generated method stub
 
 		//branchTest1 덮고싶어용~
-		System.out.println(" branchTest1");
+		System.out.println(" branchTest1sssss");
 //branchTest1 덮고싶어용~
 
 
