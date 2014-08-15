4d97e38aaf993911226c3a0c5592c324b99c4cb7
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 16:18:52 +0900

    또고쳐 브랜치 고침

--- src/mina1/mina1.java
+++ src/mina1/mina1.java
@@ -5,24 +5,18 @@ public class mina1 {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-				
-		System.out.println(" local ori1");
-		
 
-		System.out.println(" local ori26666");
-		System.out.println(" local ori1 서버 추가");
-		
-		System.out.println(" local ori24444");
-
-		System.out.println(" local ori1 서버 추가");
-		
-		System.out.println(" local ori244441");
-		
-		
-		
 		//서버 추가 20140815
 		System.out.println(" server add 1");
-	
+
+		System.out.println(" server 추가1");
+		System.out.println("`1112222222");
+		
+		System.out.println(" server 추가1");
+		System.out.println("`1112222222");
+		
+		System.out.println(" server 추가122");
+
 
 	}
 
