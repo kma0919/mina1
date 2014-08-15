2b4b1148003dc64c1670c8006479b084d4e54d3b
kma0919 <kma0919@gmail.com>
2014-08-15 15:20:11 +0900

    Update mina2.java

--- src/mina1/mina2.java
+++ src/mina1/mina2.java
@@ -6,7 +6,9 @@ public class mina2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 				
-		System.out.println(" local ori1");
+		System.out.println(" local ori2");
+		
+		System.out.println(" server add 1");
 		
 
 	}
