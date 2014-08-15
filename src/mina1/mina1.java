d09c9581d1a799852db6365af06276da17c2b5ef
fallsky0919 <fallsky0919@gmail.com>
2014-08-15 15:52:43 +0900

    새로운 브런치 커밋합니다

--- src/mina1/mina1.java
+++ src/mina1/mina1.java
@@ -12,11 +12,15 @@ public class mina1 {
 		System.out.println(" server 추가1");
 		System.out.println("`1112222222");
 		
+<<<<<<< Upstream, based on origin/master
 		System.out.println(" server 추가1");
 		System.out.println("`1112222222");
 		
 		System.out.println(" server 추가122");
 
+=======
+		//새로운 브런치 입니당당당
+>>>>>>> 3acb421 새로운 브런치 커밋합니다
 
 	}
 
